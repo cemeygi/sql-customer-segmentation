8805f2e9470a82313eabf28bace22f3b1933d9e8
cemeygi <32767483+cemeygi@users.noreply.github.com>
2025-07-04 16:23:01 +0200

    Create top_rented_movies.sql

---
+++ top_rented_movies.sql
@@ -0,0 +1,10 @@
+SELECT f.title, COUNT(r.rental_id) AS rental_count
+FROM film f
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY f.title
+ORDER BY rental_count DESC
+LIMIT 10;
+
+Description:
+An advanced query using JOIN, GROUP BY, and ORDER BY to find the top 10 most rented movies.
